3fdd3d3f9594c43b8ba1d4e819eb762af0895e34
Jack Pritchard <jack@noface.co.uk>
2019-01-03 11:07:26 +0000

    Improved archive types and limits

--- src/components/organisms/archive.jsx
+++ src/components/organisms/archive.jsx
@@ -12,9 +12,23 @@ const ArchiveElement = styled.section`
 export default class archive extends Component {
 	render() {
 		const queryCount = this.props.count;
+		const type = this.props.type;
+
 		const TeaseQuery = graphql`
 			query TeaseQuery {
 				allNoFaceCase(limit: 200) {
+					edges {
+						node {
+							id
+							excerpt
+							slug
+							thumbnailDefault
+							title
+						}
+					}
+				}
+
+				allNoFaceInsight(limit: 200) {
 					edges {
 						node {
 							id
@@ -34,17 +48,41 @@ export default class archive extends Component {
 					<ArchiveElement className="ignore">
 						<Container>
 							<Row>
-								{data.allNoFaceCase.edges.map(({ node }) => (
-									<Col sm={12} md={6} lg={4} key={node.id}>
-										<Tease
-											excerpt={node.excerpt}
-											image={node.thumbnailDefault}
-											slug={node.slug}
-											title={node.title}
-											type="case"
-										/>
-									</Col>
-								))}
+								{(() => {
+									switch (this.props.type) {
+										case "post":
+											return data.allNoFaceInsight.edges
+												.slice(0, queryCount)
+												.map(({ node }) => (
+													<Col sm={12} md={6} lg={4} key={node.id}>
+														<Tease
+															excerpt={node.excerpt}
+															image={node.thumbnailDefault}
+															slug={node.slug}
+															title={node.title}
+															type="case"
+														/>
+													</Col>
+												));
+										case "caseStudy":
+											return data.allNoFaceCase.edges
+												.slice(0, queryCount)
+												.map(({ node }) => (
+													<Col sm={12} md={6} lg={4} key={node.id}>
+														<Tease
+															excerpt={node.excerpt}
+															image={node.thumbnailDefault}
+															slug={node.slug}
+															title={node.title}
+															type="case"
+														/>
+													</Col>
+												));
+										default:
+											return "#FFFFFF";
+									}
+								})()}
+								{}
 							</Row>
 						</Container>
 					</ArchiveElement>
