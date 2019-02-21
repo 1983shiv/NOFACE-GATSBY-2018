7fd6c95e59dd9aa75cf4b4d6c75d6e0991b40b09
Jack Pritchard <jack@noface.co.uk>
2019-02-21 20:23:10 +0000

    Remove gatsby offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,6 @@ module.exports = {
 	plugins: [
 		"gatsby-plugin-catch-links",
 		"gatsby-plugin-lodash",
-		"gatsby-plugin-offline",
 		"gatsby-plugin-react-helmet",
 		"gatsby-plugin-sass",
 		"gatsby-plugin-sharp",
